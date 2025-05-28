2de2f1f3e3c240b38ff2ca3b3a41c36cd4bdd878
talhabenzaid <talhabenzaid7@gmail.com>
2025-05-28 06:06:57 +0100

    fix

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -34,6 +34,38 @@ Account::~Account( void )
               << ";amount:" << _amount
               << ";closed" << std::endl;
 
-    _nbAccounts--;
-    _totalAmount -= _amount;
+}
+
+int    Account::getNbAccounts( void )
+{
+    return(_nbAccounts);
+}
+int    Account::getTotalAmount( void )
+{
+    return(_totalAmount);
+}
+int    Account::getNbDeposits( void )
+{
+    return(_totalNbDeposits);
+}
+int    Account::getNbWithdrawals( void )
+{
+    return(_totalNbWithdrawals);
+}
+void	Account::displayAccountsInfos( void )
+{
+    _displayTimestamp();
+    std::cout << " accounts:" << getNbAccounts()
+              << ";total:" << getTotalAmount()
+              << ";deposits:" << getNbDeposits()
+              << ";withdrawals:" << getNbWithdrawals() << std::endl;
+}
+
+void	Account::displayStatus( void ) const
+{
+    _displayTimestamp();
+    std::cout << " index:" << _accountIndex
+              << ";amount:" << _amount
+              << ";deposits:" << _nbDeposits
+              << ";withdrawals:" << _nbWithdrawals << std::endl;
 }
