39980d91f910994e295f688cbfbebd49ddeb02c2
Talha Benzaid <talhabenzaid7@gmail.com>
2025-08-26 14:31:10 +0100

    done

--- cpp01/ex01/Zombie.cpp
+++ cpp01/ex01/Zombie.cpp
@@ -9,10 +9,12 @@ Zombie::~Zombie()
 {
     std::cout << name << " Destroyed\n";
 }
+
 void Zombie::set_name(std::string name)
 {
     this->name = name;
 }
+
 void Zombie::announce( void )
 {
     std::cout << name << ": BraiiiiiiinnnzzzZ...\n";
--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -1,13 +1,7 @@
 #include "Zombie.hpp"
 
-void f()
-{
-    system("leaks  Zombie");
-}
-
 int main()
 {
-    // atexit(f);
-    Zombie *zombie = zombieHorde(001,"Two_zombie");
+    Zombie *zombie = zombieHorde(2,"Two_zombie");
     delete[] zombie;
 }
