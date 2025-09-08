7a391d3e150f67c3438f8c213991622e44a8f5f9
talhabenzaid <talhabenzaid7@gmail.com>
2025-09-08 20:51:35 +0100

    push

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,58 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+    std::cout << "Default constructor called\n";
+    fixed_point = 0;
+}
+
+Fixed::Fixed(const int i)
+{
+    std::cout << "Int constructor called\n";
+    fixed_point = i << fractional_bits;
+}
+Fixed::Fixed(const float j)
+{
+    std::cout << "Int constructor called\n";
+    fixed_point = static_cast<int>(roundf(j * (1 << fractional_bits)));
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+    std::cout << "Copy constructor called\n";
+    fixed_point = other.fixed_point;
+}
+
+Fixed& Fixed::operator=( Fixed& other)
+{
+    std::cout << "Copy assignment operator called \n";
+    if(this != &other)
+        fixed_point = other.fixed_point;
+    return(*this);
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called\n";
+}
+
+void Fixed::setRawBits( int const raw )
+{
+    fixed_point = raw;
+}
+
+int Fixed::getRawBits( void ) const
+{
+    std::cout << "getRawBits member function called\n";
+    return(fixed_point);
+}
+
+float Fixed::toFloat( void ) const
+{
+    return (float)fixed_point / (1 << fractional_bits);
+}
+
+int Fixed::toInt(void) const
+{
+        return fixed_point >> fractional_bits;
+}
---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,26 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+    private:
+        int fixed_point;
+        static const int fractional_bits = 8;
+    public:
+        Fixed();
+        Fixed(const int i);
+        Fixed(const float j);
+        Fixed(const Fixed &other);
+        Fixed &operator=( Fixed &other);
+        ~Fixed();
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+        float toFloat( void ) const;
+        int toInt( void ) const;
+        
+};
+
+#endif
