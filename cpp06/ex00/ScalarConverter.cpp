3731da500fef2a95db98efb01c1f7572d5c145b6
talhabenzaid <talhabenzaid7@gmail.com>
2025-10-29 15:11:37 +0100

    push

---
+++ cpp06/ex00/ScalarConverter.cpp
@@ -0,0 +1,11 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &other){}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other)
+{
+    return *this;
+}
+ScalarConverter::~ScalarConverter() {}
---
+++ cpp06/ex00/ScalarConverter.hpp
@@ -0,0 +1,24 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <string>
+#include <iostream>
+
+
+
+class ScalarConverter
+{
+private:
+    ScalarConverter();
+    ScalarConverter(const ScalarConverter &other);
+    ScalarConverter &operator=(const ScalarConverter &other);
+    ~ScalarConverter();
+
+public:
+    static void convert(const std::string &word);
+};
+
+
+
+
+#endif
